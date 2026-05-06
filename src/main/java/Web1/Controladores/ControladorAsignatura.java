d018bf99ad29dacbb1231f4324e106f0451a1cf1
ESNEIDER MUÑOZ <esneider.munoz@correounivalle.edu.co>
2026-05-06 16:00:57 -0500

    cl3

---
+++ src/main/java/Web1/Controladores/ControladorAsignatura.java
@@ -0,0 +1,49 @@
+package Web1.Controladores;
+
+
+import java.util.ArrayList;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Sala Sistemas
+ */
+public class ControladorAsignatura {
+    private String List<codigo>;
+    private String List<nombre>;
+    private int List<creditos>;
+    private int List<intensidad>;
+
+    public ControladorAsignatura(String codigo, String nombre, int creditos, int intensidad) {
+        this.codigo = new ArrayList<>();
+        this.nombre = ArrayList<>();
+        this.creditos = ArrayList<>();
+        this.intensidad = ArrayList<>();
+    }
+
+    public String getList() {
+        return List;
+    }
+
+    public String getList() {
+        return List;
+    }
+
+    public int getList() {
+        return List;
+    }
+
+    public int getList() {
+        return List;
+    }
+
+    
+    
+    
+    
+    
+}
